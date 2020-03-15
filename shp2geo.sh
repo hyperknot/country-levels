e38c26f1cb37d761ef26c583b8c3529b68228286
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-15 17:44:43 +0100

    data/geojson working

--- shp2geo.sh
+++ shp2geo.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-rm -rf geojson
-mkdir geojson
+rm -rf data/geojson
+mkdir data/geojson
 
 yarn
 
-yarn shp2json shp/countries/*.shp -o geojson/countries.geojson --encoding utf8
-yarn shp2json shp/units/*.shp -o geojson/units.geojson --encoding utf8
-yarn shp2json shp/subunits/*.shp -o geojson/subunits.geojson --encoding utf8
-yarn shp2json shp/states/*.shp -o geojson/states.geojson --encoding utf8
+yarn shp2json data/shp/countries/*.shp -o data/geojson/countries.geojson --encoding utf8
+yarn shp2json data/shp/units/*.shp -o data/geojson/units.geojson --encoding utf8
+yarn shp2json data/shp/subunits/*.shp -o data/geojson/subunits.geojson --encoding utf8
+yarn shp2json data/shp/states/*.shp -o data/geojson/states.geojson --encoding utf8
