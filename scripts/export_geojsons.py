4d85315bb2e6f32297cb3b861acb8c93cb74b91c
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-18 00:30:41 +0100

    readme update

--- scripts/export_geojsons.py
+++ scripts/export_geojsons.py
@@ -2,16 +2,17 @@
 import shutil
 
 from country_level_lib.config import export_dir
-from country_level_lib.geojson import export_id0, export_id1, export_id2
+from country_level_lib.geojson import export_id0, export_id1, export_id2, export_id3
 
 
 def main():
     shutil.rmtree(export_dir, ignore_errors=True)
     export_dir.mkdir(parents=True)
 
-    export_id0()
-    export_id1()
-    export_id2()
+    # export_id0()
+    # export_id1()
+    # export_id2()
+    export_id3()
 
 
 if __name__ == "__main__":
