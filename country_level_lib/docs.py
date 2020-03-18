7013eabcb67e150987b7886b8ea27e60e10986cb
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-18 01:51:39 +0100

    docs updated

--- country_level_lib/docs.py
+++ country_level_lib/docs.py
@@ -4,16 +4,16 @@ from country_level_lib.utils import read_json, read_file, write_file
 
 def generate_readme():
     levels = read_json(id_dir / 'id012.json')
-    document = ''
+    tree_md = ''
 
     for id0, id0_data in levels.items():
         name = id0_data['name']
         code = id0[4:].lower()
 
-        document += (
+        tree_md += (
             f'- **{id0}** [{name}](export/geojson/id0/{code}.geojson)'
-            f'      '
-            f'[id3 list](export/id/id3/{code}.json)'
+            f'\n'
+            f'       id3 [json](export/id/id3/{code}.json)'
             f'\n'
         )
         # print(id0, id0_data)
@@ -22,16 +22,24 @@ def generate_readme():
 
         sub1 = id0_data['sub1']
         for id1, id1_data in sub1.items():
-            print('  ', id1, id1_data)
+            name = id1_data['name']
+            level = id1[:3]
+            code = id1[4:].lower()
+
+            tree_md += f'  - **{id1}** [{name}](export/geojson/{level}/{code}.geojson)\n'
             if 'sub2' not in id1_data:
                 continue
 
             sub2 = id1_data['sub2']
             for id2, id2_data in sub2.items():
-                print('   ', id2, id2_data)
+                name = id2_data['name']
+                level = id2[:3]
+                code = id2[4:].lower()
+
+                tree_md += f'  - **{id2}** [{name}](export/geojson/{level}/{code}.geojson)\n'
 
     readme_template = read_file(docs_dir / 'README_template.md')
-    readme_replaced = readme_template.replace('___REPLACE_TEMPLATE___', document)
+    readme_replaced = readme_template.replace('___REPLACE_TEMPLATE___', tree_md)
     write_file(root_dir / 'README.md', readme_replaced)
 
     print(f'Readme updated')
