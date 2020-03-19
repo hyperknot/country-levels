258da641b3f1a81b2ea688824b23a35266221521
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-20 00:29:26 +0100

    population fixes

--- scripts/wikidata_iso.py
+++ scripts/wikidata_iso.py
@@ -5,8 +5,8 @@ from country_level_lib.wikidata_iso import get_iso_id3, get_iso_id0
 
 
 def main():
-    get_iso_id0()
-    # get_iso_id3()
+    # get_iso_id0()
+    get_iso_id3()
 
 
 if __name__ == "__main__":
