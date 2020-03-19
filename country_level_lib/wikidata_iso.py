9a8fb929368b94e32c5db224e2f01cd2ca7e2145
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-19 23:55:45 +0100

    population and wikidata in id012

--- country_level_lib/wikidata_iso.py
+++ country_level_lib/wikidata_iso.py
@@ -5,13 +5,30 @@ from country_level_lib.utils import split_to_chunks, write_json
 from country_level_lib.wikidata_utils import get_results, get_all_ids, make_wd_ids_str
 
 
+def get_iso_id0():
+    all_ids = get_all_ids(get_countries=True)
+
+    data = dict()
+
+    for i, batch in enumerate(split_to_chunks(all_ids, 100)):
+        print(f'Querying wikidata ISO id0 batch: #{i+1}, {len(batch)}')
+
+        batch_data = run_query_id3(batch)
+        data = dict(data, **batch_data)
+
+        time.sleep(5)
+
+    wikidata_dir.mkdir(exist_ok=True, parents=True)
+    write_json(wikidata_dir / 'iso_id3.json', data, indent=2, sort_keys=True)
+
+
 def get_iso_id3():
     all_ids = get_all_ids(get_countries=False, get_units=False, get_subunits=False)
 
     data = dict()
 
     for i, batch in enumerate(split_to_chunks(all_ids, 100)):
-        print(f'Querying wikidata ISO batch: #{i+1}, {len(batch)}')
+        print(f'Querying wikidata ISO ido3 batch: #{i+1}, {len(batch)}')
 
         batch_data = run_query_id3(batch)
         data = dict(data, **batch_data)
