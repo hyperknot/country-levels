7717282fe85eebc91a32e812f0d7a9f60761e90f
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-20 00:48:34 +0100

    process_all fixes

--- process_all.sh
+++ process_all.sh
@@ -2,10 +2,17 @@
 set -e
 cd "${BASH_SOURCE%/*}/" || exit
 
+rm -rf data export
+mkdir data
+
 cd scripts
 
 ./download.sh
 ./shp2geo.sh
+
+./wikidata_iso.py
+./wikidata_population.py
+
 ./create_ids.py
 ./export_geojsons.py
 ./generate_docs.py
--- scripts/wikidata_iso.py
+++ scripts/wikidata_iso.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 
-from country_level_lib.wikidata_iso import get_iso_id3, get_iso_id0
+from country_level_lib.wikidata_iso import get_iso_id3
 
 
 def main():
-    # get_iso_id0()
     get_iso_id3()
 
 
