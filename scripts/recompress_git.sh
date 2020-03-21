83970f5266d97308b263e6e3d7cd6235fff95c7a
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-21 19:14:42 +0100

    recompress_git .git cleaning

--- scripts/recompress_git.sh
+++ scripts/recompress_git.sh
@@ -2,15 +2,21 @@
 set -e
 cd "${BASH_SOURCE%/*}/" || exit
 
-cd ../..
+cd ..
+DIR=$(pwd)
 
-rm -rf tmp_recompress
+cd ..
+rm -rf tmp_clean tmp_recompress*
 
 git clone --mirror git@github.com:hyperknot/country-level-id.git tmp_recompress
-cd tmp_recompress
+cd tmp_recompress || exit
 bfg -b 4K -D '*.geojson'
 git reflog expire --expire=now --all && git gc --prune=now --aggressive
 git push
+cd .. || exit
 
-cd ..
-rm -r tmp_recompress
+git clone git@github.com:hyperknot/country-level-id.git tmp_clean
+rm -rf "$DIR/.git"
+mv tmp_clean/.git "$DIR"
+
+rm -rf tmp_clean tmp_recompress*
