3b972ab60a554967fc8d728c9a836395dd829dc4
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-21 17:36:19 +0100

    geojson-precision

--- scripts/topo_simplify.sh
+++ scripts/topo_simplify.sh
@@ -14,15 +14,20 @@ yarn geo2topo \
     states=$DIR/states.geojson \
     -o $DIR/ne-topo.json
 
-for i in 5 6 7 8
+for i in 5 7 8
 do
   yarn toposimplify -s 1e-$i -o $DIR/ne-topo-simp-$i.json $DIR/ne-topo.json
-  yarn topoquantize 1e5 -o $DIR/ne-topo-simp-$i-q.json $DIR/ne-topo-simp-$i.json
-  yarn topo2geo -i $DIR/ne-topo-simp-$i-q.json \
+  yarn topo2geo -i $DIR/ne-topo-simp-$i.json \
     countries=$DIR/countries-$i.geojson \
     units=$DIR/units-$i.geojson \
     subunits=$DIR/subunits-$i.geojson \
     states=$DIR/states-$i.geojson
+
+  yarn geojson-precision -p 3 $DIR/countries-$i.geojson $DIR/countries-$i.geojson
+  yarn geojson-precision -p 3 $DIR/units-$i.geojson $DIR/units-$i.geojson
+  yarn geojson-precision -p 3 $DIR/subunits-$i.geojson $DIR/subunits-$i.geojson
+  yarn geojson-precision -p 3 $DIR/states-$i.geojson $DIR/states-$i.geojson
 done
 
 rm $DIR/*.json
+
