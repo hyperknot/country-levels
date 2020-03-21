f503b67d725f250a36487c9b11cd45eb2cd7afb8
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-21 19:06:20 +0100

    recompress git added

--- scripts/recompress_git.sh
+++ scripts/recompress_git.sh
@@ -9,3 +9,8 @@ rm -rf tmp_recompress
 git clone --mirror git@github.com:hyperknot/country-level-id.git tmp_recompress
 cd tmp_recompress
 bfg -b 4K -D '*.geojson'
+git reflog expire --expire=now --all && git gc --prune=now --aggressive
+git push
+
+cd ..
+rm -r tmp_recompress
