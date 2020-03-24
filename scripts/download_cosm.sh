436e976f4f43791a1b64e823d5a23cbbcb331038
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-24 12:52:56 +0100

    cosm fix

--- scripts/download_cosm.sh
+++ scripts/download_cosm.sh
@@ -3,12 +3,13 @@ set -e
 cd "${BASH_SOURCE%/*}/" || exit
 
 cd ../data
-rm -rf tmp data/geojson/cosm
-mkdir -p tmp data/geojson/cosm
+
+rm -rf tmp geojson/cosm
+mkdir -p tmp geojson/cosm
 
 wget https://github.com/osm-without-borders/cosmogony/releases/download/v0.7.3/cosmogony-2020-03-02-regions.jsonl.gz -O tmp/cosm.jsonl.gz
 
 gunzip tmp/cosm.jsonl.gz
-mv tmp/cosm.jsonl data/geojson/cosm/cosm.jsonl
+mv tmp/cosm.jsonl geojson/cosm/cosm.jsonl
 
 rm -r tmp
