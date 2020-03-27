8cfda49fd97f4fe4c127a32fd02f136a6f3e04e5
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-27 14:08:40 +0100

    release script

---
+++ scripts/release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+cd "${BASH_SOURCE%/*}/" || exit
+
+cd ..
+
+rm -rf release
+
+for i in 5 7 8
+do
+  mkdir -p release/q$i/geojson
+  cp export/*.json release/q$i
+  cp -R export/geojson/q$i/ release/q$i/geojson
+  cd release/q$i || exit
+  7z a ../export_q$i.zip "*"
+  cd ../..
+done
