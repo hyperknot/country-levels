1ec53442e9380f0f7ed3255ead101e5151c4f6c5
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-31 22:57:03 +0200

    use census data for US states

--- scripts/wam_all.sh
+++ scripts/wam_all.sh
@@ -2,6 +2,7 @@
 set -e
 cd "${BASH_SOURCE%/*}/" || exit
 
+./fips_download_csv.sh
 ./wam_download.py
 ./wam_collect.py
 ./wam_topo_simplify.sh
--- scripts/wam_export.py
+++ scripts/wam_export.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
+import shutil
 
-from country_levels_lib.wam_export import export_wam
+from country_levels_lib import wam_export
+from country_levels_lib.config import export_dir
+from country_levels_lib.fips_utils import get_population_by_state
 
 
 def main():
-    export_wam()
+    shutil.rmtree(export_dir / 'geojson', ignore_errors=True)
+
+    # for simp in [5, 7, 8]:
+    #     wam_export.split_geojson(1, simp, debug=False)
+    #     wam_export.split_geojson(2, simp, debug=False)
+
+    wam_export.split_geojson(2, 5, debug=False)
 
 
 if __name__ == "__main__":
