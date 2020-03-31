004929b1194e3b2b2dcfcfe68e9a7ae5c289d122
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-31 23:05:40 +0200

    correct osm us state population from census

--- scripts/wam_export.py
+++ scripts/wam_export.py
@@ -3,17 +3,14 @@ import shutil
 
 from country_levels_lib import wam_export
 from country_levels_lib.config import export_dir
-from country_levels_lib.fips_utils import get_population_by_state
 
 
 def main():
     shutil.rmtree(export_dir / 'geojson', ignore_errors=True)
 
-    # for simp in [5, 7, 8]:
-    #     wam_export.split_geojson(1, simp, debug=False)
-    #     wam_export.split_geojson(2, simp, debug=False)
-
-    wam_export.split_geojson(2, 5, debug=False)
+    for simp in [5, 7, 8]:
+        wam_export.split_geojson(1, simp, debug=False)
+        wam_export.split_geojson(2, simp, debug=False)
 
 
 if __name__ == "__main__":
