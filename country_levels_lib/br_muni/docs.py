80ad422fa7a5d5ddf4d40f10a3d0c58978ccc73f
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-04 22:13:22 +0200

    br_muni docs added

---
+++ country_levels_lib/br_muni/docs.py
@@ -0,0 +1,38 @@
+from country_levels_lib.config import export_dir, docs_dir
+from country_levels_lib.utils import read_json, write_file
+
+
+def generate_br_muni_list():
+    br_muni_json = read_json(export_dir / 'br_muni.json')
+    counties = sorted(br_muni_json.values(), key=lambda k: k['name'])
+
+    doc_md = f'# Brazil municipality IBGE code list\n'
+    doc_md += '[GeoJSON for all counties](../export/geojson/q5/br_muni_all.geojson)'
+
+    county_by_state = {}
+    for item in sorted(counties, key=lambda k: k['state_code']):
+        state_code = item['state_code']
+        county_by_state.setdefault(state_code, [])
+        county_by_state[state_code].append(item)
+
+    for state_code, state_items in county_by_state.items():
+        doc_md += f'\n\n#### {state_code}\n'
+        doc_md += 'Name | IBGE code | GeoJSON | population \n'
+        doc_md += '--- | --- | --- | --: \n'
+
+        for item in sorted(state_items, key=lambda k: k['name']):
+            ibge_code = item['ibge_code']
+            name_long = item['name_long']
+            population = item['population']
+
+            population_str = ''
+            if population:
+                population_str = f'{population:,}'
+
+            geojson_link = (
+                f'[GeoJSON](../export/geojson/q8/br_muni/{state_code}/{ibge_code}.geojson)'
+            )
+
+            doc_md += f'{name_long} | {ibge_code} | {geojson_link} | {population_str}\n'
+
+    write_file(docs_dir / 'br_muni_list.md', doc_md)
---
+++ scripts/br_muni/docs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from country_levels_lib.br_muni.docs import generate_br_muni_list
+
+
+def main():
+    generate_br_muni_list()
+
+
+if __name__ == "__main__":
+    main()
