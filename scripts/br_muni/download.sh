228a4f3ab23d889d0ffc5b948c4e165f1d60ed6b
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-04 21:29:17 +0200

    br_muni simplify done

--- scripts/br_muni/download.sh
+++ scripts/br_muni/download.sh
@@ -4,7 +4,8 @@ cd "${BASH_SOURCE%/*}/" || exit
 
 cd ../..
 
-rm -rf data/geojson/br_muni
-mkdir -p data/geojson/br_muni
+DIR=data/geojson/br_muni
+rm -rf $DIR
+mkdir -p $DIR/orig
 
-wget https://raw.githubusercontent.com/thiagoghisi/br-counties-geodata/master/br-counties-min.geojson -O data/geojson/br_muni/original.geojson
+wget https://raw.githubusercontent.com/thiagoghisi/br-counties-geodata/master/br-counties-min.geojson -O $DIR/orig/orig.geojson
---
+++ scripts/br_muni/topo_simplify.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+cd "${BASH_SOURCE%/*}/" || exit
+
+cd ../..
+
+GEOJSON_ORIG=data/geojson/br_muni/orig
+GEOJSON_SIMP=data/geojson/br_muni/simp
+TOPOJSON=data/topojson/br_muni
+
+rm -rf $TOPOJSON $GEOJSON_SIMP
+mkdir -p $TOPOJSON $GEOJSON_SIMP
+
+node --max-old-space-size=40000 node_modules/.bin/geo2topo \
+  --quantization 1e8 \
+  g=$GEOJSON_ORIG/orig.geojson \
+  -o $TOPOJSON/topo.json
+
+for i in 5 7 8
+do
+  node --max-old-space-size=40000 node_modules/.bin/toposimplify \
+    -s 1e-$i -o $TOPOJSON/simp-$i.json $TOPOJSON/topo.json
+
+  node --max-old-space-size=40000 node_modules/.bin/topo2geo \
+    -i $TOPOJSON/simp-$i.json \
+    g=$GEOJSON_SIMP/simp-$i.geojson
+
+  node --max-old-space-size=40000 node_modules/.bin/geojson-precision \
+    -p 3 $GEOJSON_SIMP/simp-$i.geojson $GEOJSON_SIMP/simp-$i.geojson
+done
+
+rm -rf $TOPOJSON $GEOJSON_ORIG
+
+
