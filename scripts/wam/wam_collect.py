bd9c60b8bf6a7863ff2fab08723eab5b83fb03e2
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-07 00:12:42 +0200

    no save_population in wam_collect.py

--- scripts/wam/wam_collect.py
+++ scripts/wam/wam_collect.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-from country_levels_lib.wam.wam_collect import collect_iso, save_population
+from country_levels_lib.wam.wam_collect import collect_iso
 
 
 def main():
     collect_iso()
-    save_population()
 
 
 if __name__ == "__main__":
