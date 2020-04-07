e4ac2950a6894d48fd7c8e720980c6be461118ac
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-07 22:07:29 +0200

    fips all.sh fixes

--- scripts/fips/all.sh
+++ scripts/fips/all.sh
@@ -2,7 +2,8 @@
 set -e
 cd "${BASH_SOURCE%/*}/" || exit
 
-./fips_download_csv.sh
-./fips_download_shp.sh
-./fips_shp2geo.sh
-./fips_export.py
+./download_csv.sh
+./download_shp.sh
+./shp2geo.sh
+./export.py
+./docs.py
