06416e187ee09a546958d4c97af78bb08cab9003
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-07 17:51:21 +0200

    fix wd_ids_collected.json

--- scripts/wam/export.py
+++ scripts/wam/export.py
@@ -3,11 +3,11 @@ import shutil
 
 from country_levels_lib.config import export_dir
 from country_levels_lib.wam import wam_export
-from country_levels_lib.wam.wam_collect import save_population
+from country_levels_lib.wam.wam_collect import save_wam_population
 
 
 def main():
-    save_population()
+    save_wam_population()
 
     shutil.rmtree(export_dir / 'geojson', ignore_errors=True)
 
