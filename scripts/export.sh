44398f1dd0a191221e36d71fdce5f5d69bff0c1d
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-08 18:56:51 +0200

    export add date to commit

--- scripts/export.sh
+++ scripts/export.sh
@@ -8,9 +8,11 @@ cp ../country-levels/docs/export_readme.md README.md
 
 rm -rf .git
 
+DATE=$(date -u +%Y-%m-%dT%H:%M)
+
 git init
 git add .
-git commit -m "re-exported"
+git commit -m "new export at $DATE"
 git remote add origin git@github.com:hyperknot/country-levels-export.git
 git push -u origin master --force
 
