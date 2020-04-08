0001ef54f38762448b5e58ede1c57c9761933dca
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-08 02:33:41 +0200

    regenerated

--- scripts/wam/all.sh
+++ scripts/wam/all.sh
@@ -5,7 +5,7 @@ cd "${BASH_SOURCE%/*}/" || exit
 # ./download.py
 
 ../fips/download_csv.sh
-python -u collect.py | tee collect.txt
-./topo_simplify.sh
+# python -u collect.py | tee collect.txt
+# ./topo_simplify.sh
 python -u export.py | tee export.txt
 ./docs.py
