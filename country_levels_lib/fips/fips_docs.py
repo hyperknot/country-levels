4ea70a7fe0aadee95e0f7f42d5479f864b64ae9b
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-09 03:15:11 +0200

    fips docs fixed

--- country_levels_lib/fips/fips_docs.py
+++ country_levels_lib/fips/fips_docs.py
@@ -34,7 +34,7 @@ def generate_fips_list():
             if population:
                 population_str = f'{population:,}'
 
-            geojson_link = f'[GeoJSON](../export/geojson/q8/{geojson_path})'
+            geojson_link = f'[GeoJSON](../geojson/q8/{geojson_path})'
 
             doc_md += f'{name_long} | {fips} | {geojson_link} | {population_str}\n'
 
