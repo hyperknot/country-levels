b82aba11c56202a397f9240e6d122f93e86efdd2
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-11 15:09:12 +0200

    removed debug option

--- scripts/wam/export.py
+++ scripts/wam/export.py
@@ -8,8 +8,8 @@ def main():
     save_wam_population()
 
     for simp in [5, 7, 8]:
-        wam_export.split_geojson(1, simp, debug=False)
-        wam_export.split_geojson(2, simp, debug=False)
+        wam_export.split_geojson(1, simp)
+        wam_export.split_geojson(2, simp)
 
 
 if __name__ == "__main__":
