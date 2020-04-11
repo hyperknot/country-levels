8d87c054e96c3eac1c7fd80264556e5639a930ba
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-11 16:51:55 +0200

    rm -rf added to process_all

--- scripts/process_all.sh
+++ scripts/process_all.sh
@@ -2,6 +2,10 @@
 set -e
 cd "${BASH_SOURCE%/*}/" || exit
 
+rm -rf ../data/{fips,shp,topojson,wam}
+rm -rf ../data/geojson/{br_muni,fips}
+rm -rf ../data/geojson/wam/simp
+
 rm -rf ../../country-levels-export
 
 ./wam/all.sh
