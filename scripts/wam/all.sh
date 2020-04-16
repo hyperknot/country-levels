c014cece97de97794d55692175d42a10958acc7d
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-16 17:51:17 +0200

    remove comment

--- scripts/wam/all.sh
+++ scripts/wam/all.sh
@@ -4,7 +4,7 @@ cd "${BASH_SOURCE%/*}/" || exit
 
 # ./download.py
 
-#../fips/download_csv.sh
+../fips/download_csv.sh
 [ -z "$SKIP_COLLECT" ] && python -u collect.py | tee collect.txt
 [ -z "$SKIP_COLLECT" ] && ./topo_simplify.sh
 python -u export.py | tee export.txt
