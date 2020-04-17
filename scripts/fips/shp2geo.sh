e5a770d40b4522f2d9f6320eea721adaf94ee4be
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-17 21:19:40 +0200

    use geojson-precision for fips

--- scripts/fips/shp2geo.sh
+++ scripts/fips/shp2geo.sh
@@ -16,4 +16,8 @@ yarn shp2json $SHP/counties_500k/*.shp -o $GEOJSON/counties_500k.geojson --encod
 yarn shp2json $SHP/counties_5m/*.shp -o $GEOJSON/counties_5m.geojson --encoding utf8
 yarn shp2json $SHP/counties_20m/*.shp -o $GEOJSON/counties_20m.geojson --encoding utf8
 
+yarn geojson-precision -p 3 $GEOJSON/counties_500k.geojson $GEOJSON/counties_500k.geojson
+yarn geojson-precision -p 3 $GEOJSON/counties_5m.geojson $GEOJSON/counties_5m.geojson
+yarn geojson-precision -p 3 $GEOJSON/counties_20m.geojson $GEOJSON/counties_20m.geojson
+
 rm -r $SHP
