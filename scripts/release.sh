6df7fc6a13fbeff0189516402394fd70d74efab0
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-25 13:47:59 +0200

    json-only release added

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,14 +8,15 @@ echo '{"name": "country-levels","version": "1.0.0"}' > package.json
 
 rm -rf release
 
-for simp in low medium high
+for simp in low medium high json
 do
   mkdir -p release/$simp/geojson
   cp *.json release/$simp
   cp *.md release/$simp
-  cp -R geojson/$simp/. release/$simp/geojson
+  [ $simp != "json" ] && cp -R geojson/$simp/. release/$simp/geojson
   cd release/$simp || exit
   tar -czvf ../export_$simp.tgz .
   cd ../..
   rm -rf release/$simp
 done
+
